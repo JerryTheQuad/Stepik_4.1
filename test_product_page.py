8d1d0fd3985fe7d3173e5dd2583591a548e29cfd
JerryTheQuad <valeranikitenkov@gmail.com>
2023-06-11 16:43:25 +0300

    Added classes to test functions

--- test_product_page.py
+++ test_product_page.py
@@ -3,51 +3,52 @@ import pytest
 from .pages.basket_page import BasketPage
 from .pages.login_page import LoginPage
 
-@pytest.mark.need_review
-def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = BasketPage(browser, link)
-    page.open()
-    page.go_to_basket()
-    page.no_item_basket()
-    page.basket_is_empty()
+class TestGuestProductPage():
+    @pytest.mark.need_review
+    def test_guest_cant_see_product_in_basket_opened_from_product_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = BasketPage(browser, link)
+        page.open()
+        page.go_to_basket()
+        page.no_item_basket()
+        page.basket_is_empty()
 
-@pytest.mark.need_review
-def test_guest_can_add_product_to_basket(browser):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
-    page = ProductPage(browser, link)
-    page.open()
-    page.add_cart()
+    @pytest.mark.need_review
+    def test_guest_can_add_product_to_basket(self, browser):
+        link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_cart()
 
-@pytest.mark.need_review
-def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.go_to_login_page()
+    @pytest.mark.need_review
+    def test_guest_can_go_to_login_page_from_product_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.go_to_login_page()
 
+class TestUserProductPage():
+    @pytest.mark.need_review
+    def add_new_user(self, browser):
+        link = 'https://selenium1py.pythonanywhere.com/accounts/login/'
+        page = LoginPage(browser, link)
+        page.open()
+        page.register_new_user()
 
-@pytest.mark.need_review
-def add_new_user(browser):
-    link = 'https://selenium1py.pythonanywhere.com/accounts/login/'
-    page = LoginPage(browser, link)
-    page.open()
-    page.register_new_user()
+    def test_user_cant_see_success_message_after_adding_product_to_basket(self, browser):
+        link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_be_authorized_user()
+        page.add_cart()
+        page.should_not_be_success_message()
 
-def test_user_cant_see_success_message_after_adding_product_to_basket(browser):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
-    page = ProductPage(browser, link)
-    page.open()
-    page.should_be_authorized_user()
-    page.add_cart()
-    page.should_not_be_success_message()
-
-@pytest.mark.need_review
-def test_user_can_add_product_to_basket(browser):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
-    page = ProductPage(browser, link)
-    page.open()
-    page.add_cart()
+    @pytest.mark.need_review
+    def test_user_can_add_product_to_basket(self, browser):
+        link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_cart()
     
 
 def test_item_name_cart_identical(browser):
